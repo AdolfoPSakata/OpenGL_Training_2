45a19600d226bcd37fdf62a305d7ba88fc510aa1
adolfo.sakata <adolfo.sakata@K0080>
2024-10-02 13:51:52 -0300

    GLFW and GLEW setup

--- OpenGL_Udemy/OpenGL_Udemy/Source/Application.cpp
+++ OpenGL_Udemy/OpenGL_Udemy/Source/Application.cpp
@@ -1,6 +1,31 @@
-#include <GL/>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
-int main()
+int main(void)
 {
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+    window = glfwCreateWindow(800, 600, "OpenGl - Udemy", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
